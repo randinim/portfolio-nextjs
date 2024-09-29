563f4801f01945e2cdd8913a8feb8241f6b29a1a
Randini Maliksha <maliksharandini@gmail.com>
2024-09-29 12:02:33 +0530

    costomize portforlio

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "About",
+  //   description: 'Read my blog.',
+};
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">About</h1>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -6,26 +6,28 @@ export default function Page() {
     <section>
       <div className="flex gap-x-4 items-center flex-row mb-10">
         <Image
-          src={"/image.jpg"}
+          src={"/randi.jpeg"}
           alt="profile pic"
           width={100}
           height={100}
           className="rounded-full border-[2px] border-yellow-500 -ml-2 hover:grayscale transition-all duration-300"
         />
         <div>
-          <p className="text-xl font-semibold">John Doe</p>
+          <p className="text-xl font-semibold">Randini Maliksha</p>
           <p className="text-lg text-gray-400">Student at SLIIT</p>
         </div>
       </div>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter text-green-400">
         Welcome! 👋
       </h1>
       <p className="mb-4">
-        I am a passionate JavaScript developer and full-time student at SLIIT.
-        With a year of self-taught coding experience, I enjoy building web
-        applications and writing about web development as a part-time blogger.
-        I'm a fan of open-source contributions and JAMstack technologies, always
-        seeking new opportunities to collaborate and grow as a developer.
+        I am a passionate Java developer and full-time student at SLIIT. With
+        over a year of hands-on coding experience, I enjoy building web
+        applications using the MERN stack and continuously exploring the latest
+        technologies in the Java ecosystem. As a part-time blogger, I write
+        about software development and enjoy contributing to open-source
+        projects. I'm always looking for opportunities to collaborate, share
+        knowledge, and enhance my skills as a developer.
       </p>
       <div className="my-8">
         <p className="text-gray-400 text-lg mb-2">From my blog</p>
